9741ea152cb1aa5ef7877e24e2b8db166b99a009
biffbaff64 <richikin@gmail.com>
2020-03-31 22:35:35 +0100

    Initial commit

---
+++ android/src/com/red7projects/dungeon/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.red7projects.dungeon;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.red7projects.dungeon.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
---
+++ desktop/src/com/red7projects/dungeon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.red7projects.dungeon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.red7projects.dungeon.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
